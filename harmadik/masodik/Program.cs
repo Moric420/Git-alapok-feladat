e6d8b0639092ac5e8ae08f224c402bda2c5fb752
Moric420 <153609546+Moric420@users.noreply.github.com>
2023-12-18 12:39:41 +0100

    Harmadik
    Harmadik feladat

---
+++ harmadik/masodik/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masodik
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> szamok = new List<int>();
+
+			Console.WriteLine("Adjon meg számokat a listához! (0 a befejezéshez)");
+
+			while (true)
+			{
+				Console.Write("Kérem adjon meg egy számot: ");
+				string input = Console.ReadLine();
+
+				// Ellenőrizzük, hogy a felhasználó valóban számot adott-e meg
+				if (int.TryParse(input, out int szam))
+				{
+					// Ha 0-t adott meg, kilépünk a ciklusból
+					if (szam == 0)
+						break;
+
+					// Hozzáadjuk a számot a listához
+					szamok.Add(szam);
+				}
+				else
+				{
+					Console.WriteLine("Érvénytelen bemenet! Kérem csak számokat adjon meg.");
+				}
+			}
+
+			// Kiírjuk a listában lévő számokat
+			Console.WriteLine("A megadott számok:");
+			foreach (var szam in szamok)
+			{
+				Console.Write(szam + ", ");
+			}
+
+			Console.ReadLine(); // Várakozás a leállítás előtt
+		}
+	}
+}
