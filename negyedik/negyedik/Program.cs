77f18a405bb62bf9fd49046560ed4ba93cb1a499
Moric420 <153609546+Moric420@users.noreply.github.com>
2023-12-18 12:40:23 +0100

    Negyedik
    Negyedik feladat

---
+++ negyedik/negyedik/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negyedik
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> szamok = new List<int>();
+
+			Console.WriteLine("Adjon meg számokat a listához! (0 a befejezéshez)");
+
+			while (true)
+			{
+				Console.Write("Kérem adjon meg egy számot: ");
+				string input = Console.ReadLine();
+
+				if (int.TryParse(input, out int szam))
+				{
+					if (szam == 0)
+						break;
+
+					szamok.Add(szam);
+				}
+				else
+				{
+					Console.WriteLine("Érvénytelen bemenet! Kérem csak számokat adjon meg.");
+				}
+			}
+
+			// Ellenőrizzük, hogy van-e szám a listában, mielőtt a Minimum metódust használnánk
+			if (szamok.Any())
+			{
+				// Használjuk a Min metódust a legkisebb elem kiválasztásához
+				int legkisebbElem = szamok.Min();
+
+				Console.WriteLine($"A legkisebb elem a listában: {legkisebbElem}");
+			}
+			else
+			{
+				Console.WriteLine("Nincs megadott szám a listában.");
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
